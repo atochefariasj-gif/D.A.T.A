b75f9f1ab76c964d5062c869e25f08bedbff06d7
atochefariasj-gif <atochefariasj@gmail.com>
2026-08-23 00:23:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2080,8 +2080,8 @@ const worksheet = workbook.Sheets[targetSheetName];
         
         // 🔑 Reemplaza la primera celda sin importar si tiene rowspan o colspan
         const styledHtml = htmlTableRaw
-            .replace('<table', '<table style="width:100%; border-collapse: collapse; font-size: 10px; font-family: Arial, sans-serif; background: #fff;"')
-            .replace(/<tr>\s*<td[^>]*>.*?<\/td>/s, '<tr><td style="border: 1px solid #000; text-align: center; vertical-align: middle; padding: 4px;"><img src="logo.png" alt="Logo Agromar" style="max-height: 35px; width: auto; display: block; margin: 0 auto;" /></td>');
+    .replace('<table', '<table style="width:100%; border-collapse: collapse; font-size: 10px; font-family: Arial, sans-serif; background: #fff;"')
+    .replace(/<tr>\s*<td[^>]*>.*?<\/td>/s, '<tr><td style="border: 1px solid #000; text-align: center; vertical-align: middle; padding: 4px;"><img src="logo.png" alt="Logo Agromar" style="max-height: 35px; width: auto; display: block; margin: 0 auto;" /></td>');
         // Mostrar en pantalla
         const container = document.getElementById('kardex-excel-table-container');
         if (container) {
